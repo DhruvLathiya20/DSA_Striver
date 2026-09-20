470992c7cfcbe8f7bd893b550d365e593f12530e
DhruvLathiya20 <dhruvlathiya077@gmail.com>
2026-09-20 16:02:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.size();
         int sum=0;
         for(int i=0;i<n;i++){
-            sum=sum+(i+1)*('z'-s[i]+1);
+            sum+=(26-(s[i]-'a'))*(i+1);
         }
         return sum;
         
